20fa5b7b43e7576a777f25d83678a78c2c36819b
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2022-12-16 11:35:44 +0200

    with spring data

---
+++ src/main/java/com/backend/StudentManagement/models/Student.java
@@ -0,0 +1,43 @@
+package com.backend.StudentManagement.models;
+
+import com.backend.StudentManagement.util.*;
+import org.hibernate.validator.constraints.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.*;
+import java.util.*;
+@Entity
+@Table(name="student")
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, updatable = false)
+    private Date createdAt = Dates.nowUTC();
+
+    @NotEmpty
+    @Length(max = 60)
+    private String fullname;
+
+    private Date birthDate;
+
+    @Min(100)
+    @Max(800)
+    private Integer satScore;
+
+    @Min(30)
+    @Max(110)
+    private Double graduationScore;
+
+    @Length(max = 20)
+    private String phone;
+
+    @Length(max = 500)
+    private String profilePicture;
+
+}
